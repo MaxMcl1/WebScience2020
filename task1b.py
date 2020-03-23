1d9c2dc3d1b7ff4275e3875967ff941d27746b24
2137949m <2137949m@student.gla.ac.uk>
2020-03-23 11:19:41 +0000

    removed keys

--- task1b.py
+++ task1b.py
@@ -1,8 +1,11 @@
+#Task 1b
+
 import tweepy
 import json
 import time
 
 from datetime import datetime
+from datetime import timedelta
 from tweepy import StreamListener
 from pymongo import MongoClient
 
@@ -13,7 +16,7 @@ access_token_secret = 'ENTER ACCESS TOKEN SECRET'
 
 client = MongoClient('localhost', 27017)
 db = client['local']
-collection = db['Crawler 1a']
+collection = db['Crawler 1b']
 
 LOCATIONS = [6.7499552751, 36.619987291, 18.4802470232, 47.1153931748, #Italy
             -7.57216793459, 49.959999905, 1.68153079591, 58.6350001085, #United Kingdom
@@ -25,39 +28,57 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+RUN_TIME = 30 # minutes
+
+start_time = datetime.now()
+end_time = start_time + timedelta(minutes=RUN_TIME)
+
+def convert_to_datetime(status):
+    t = status._json
+    t['created_at'] = datetime.strptime(t['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+    t['created'] = t.pop('created_at', None)
+    return t
+
 class CustomStreamListener(tweepy.StreamListener):
-    def __init__(self):
-        super().__init__()
-        #print("Here")
 
     def on_status(self, status):
-        t = json.loads(status)
-        dt = t['created_at']
-        dt = datetime.strptime(dt, '%a %b %d %H:%M:%S +0000 %Y')
-        collection.insert(t)
-
-    def on_data(self, data):
-        t = json.loads(data)
-        collection.insert(t)
+        collection.insert(convert_to_datetime(status))
+        return True
+     
 
     def on_error(self, status):
+        if status == 420:
+            return False
         print (status)
-        return True
-
-    def on_timeout(self):
-        print("Timeout")
-        return True
 
 listenerStream = CustomStreamListener()
 streamer = tweepy.Stream(auth=auth, listener=listenerStream)
 
-# 1a) Simple crawler for collecting 1% data - Started 11:39 9/03/2020 
-#streamer.filter()
-#streamer.sample()
-
 # 1b) Enhanced Crawler, filtering on TRENDS - Started 11:48 9/03/2020
 TRENDS = ['#coronavirus', 'cases', 'spread', 'NHS']
-streamer.filter(track= TRENDS, locations=LOCATIONS, languages=['en'])
+streamer.filter(track= TRENDS, locations=LOCATIONS, languages=['en'], is_async=True)
+#streamer.sample(languages=["en"], is_async = True)
+
+#query = "Coronavirus"
+
+while datetime.now() < end_time:
+    time.sleep(30)
+
+streamer.disconnect()
+
+print("Start time: ", start_time)
+print("End time: ", end_time)
+
+#t_rest = api.search(q = query, lang=['en'], count = 50)
+
+# #print(t_rest)
+
+#tweets = t_rest['statuses']
+
+#for i in tweets:
+#    i['created_at'] = datetime.strptime(i['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+#    i['created'] = i.pop('created_at', None)
+#    collection.insert(i)
 
 
 
